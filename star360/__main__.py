6f1b3101ea310028a3c7aad14714c6da955e408e
Jack DeVries <jdevries3133@gmail.com>
2021-02-04 17:46:40 -0500

    feat: star360 mail merge scripts

---
+++ star360/__main__.py
@@ -0,0 +1,28 @@
+import logging
+from pathlib import Path
+import sys
+
+from .email import Star360MailMerge
+
+BASE_DIR = Path(__file__).parent
+
+logger = logging.getLogger(__name__)
+
+mailer = Star360MailMerge(
+    Path(Path(__file__).parent, 'star360_credentials.csv'),
+    debug=False,
+)
+try:
+    with open(Path(BASE_DIR, 'sent.txt'), 'r') as txtf:
+        sent = txtf.read()
+        sent = [n.strip() for n in sent.split('\n')]
+    mailer.skip = sent
+    mailer.send_emails()
+except Exception as e:
+    logger.exception('exception')
+    with open(Path(BASE_DIR, 'sent.txt'), 'a') as txtf:
+        txtf.writelines([l + '\n' for l in mailer.sent_to])
+    sys.exit()
+
+with open(Path(BASE_DIR, 'sent.txt'), 'w') as txtf:
+    txtf.write('')
--- star360/email.py
+++ star360/email.py
@@ -7,12 +7,13 @@ from datetime import datetime
 import sys
 from pathlib import Path
 import logging
+from time import sleep
 
 from teacherHelper import Helper, Email
 
 logging.basicConfig(
-    filename="star360_emailer.log",
-    level=logging.INFO
+    filename=Path(Path(__file__).parent, "star360_emailer.log"),
+    level=logging.DEBUG
 )
 logger = logging.getLogger(__name__)
 
@@ -23,10 +24,14 @@ class StudentNotFound(Exception):
 
 class Star360MailMerge:
 
-    def __init__(self, csv_path: Path, debug=True):
+    def __init__(self, csv_path: Path, debug=True, skip=None):
         self.debug = debug   # sends all emails to me
         self.DEBUG_EMAIL = 'jdevries3133@gmail.com'
         self.csv = csv_path
+        # allow the ability to skip some students on a second run after smtp
+        # kick. This should be a list of exact-match student names.
+        self.skip = skip if skip else []
+        self.sent_to = []  # successfully sent; a list of names
 
         # saving myself from myself
         if not self.debug:
@@ -43,60 +48,56 @@ class Star360MailMerge:
             assert next(rd) == ['name', 'username', 'password']
             for name, username, password in rd:
                 st = helper.find_nearest_match(name, auto_yes=True)
+                if st.name not in ['Louis Conyers']:
+                    continue
                 if not st:
                     raise StudentNotFound(f'{name} not found')
                 st.username = username
                 st.password = password
-                self.students.append(st)
+                # LIMIT RECIPIENTS TO THE FOURTH GRADE.
+                if st.grade_level == 4:
+                    self.students.append(st)
 
     def send_emails(self):
         # TODO: break up this big function
         sent_to = []
-        try:
-            with Email() as eml:
-                for st in self.students:
-                    if self.debug:
-                        recipient = self.DEBUG_EMAIL
-                    else:
-                        recipient = st.email
-                    message = [
-                        f'Hi {st.first_name},',
-                        '',
-                        'Today, you will be taking your Star360 Assessment. '
-                        'Please complete the steps below:',
-                        '',
-                        '1. Click on this link: <a href="https://global-zone08.renaiss'
-                        'ance-go.com/welcomeportal/6234531">https://global-zone08.rena'
-                        'issance-go.com/welcomeportal/6234531</a>',
-                        '2. Click "I\'m a Student"',
-                        f'3. Type your Username: {st.onc}',
-                        f'4. Type your Password: {st.passwd}',
-                        '5. Click "Log In"',
-                        '6. Find the Star Math or Reading test (depending on teacher '
-                        'directions).',
-                        '7. Begin your test. If you need to enter a monitor password, '
-                        'type <b>Admin</b>',
-                        '',
-                        '<span style="color: red;">STAY ON THE ZOOM UNTIL WE CONFIRM '
-                        'YOU SUBMITTED YOUR TEST</span>',
-                        '',
-                        'Good luck!',
-                    ]
-                    eml.send(
-                        to=recipient,
-                        subject='Star360 Username and Password',
-                        message=message
-                    )
-                    logger.info(f'Email sent to {st.name} including username\t{st.onc}')
-                    sent_to.append(st)
-        except Exception as e:
-            logger.critical(f'Program crashed due to {e}')
-            with open(f'sent {datetime.now()}.csv', 'w') as csvf:
-                wr = csv.writer(csvf)
-                for st in sent_to:
-                    wr.writerow((st.name,))
-            print(
-                'Program crashed. See logs for details. Students who recieved '
-                'an email have been saved in ./sent.csv'
-            )
-            sys.exit()
+        with Email() as eml:
+            for i, st in enumerate(self.students):
+                if st.name in self.skip:
+                    continue
+                if self.debug:
+                    recipient = self.DEBUG_EMAIL
+                else:
+                    recipient = st.email
+                message = [
+                    f'Hi {st.first_name},',
+                    '',
+                    'Today, you will be taking your Star360 Assessment. '
+                    'Please complete the steps below:',
+                    '',
+                    '1. Click on this link: <a href="https://global-zone08.renaiss'
+                    'ance-go.com/welcomeportal/6234531">https://global-zone08.rena'
+                    'issance-go.com/welcomeportal/6234531</a>',
+                    '2. Click "I\'m a Student"',
+                    f'3. Type your Username: {st.username}',
+                    f'4. Type your Password: {st.password}',
+                    '5. Click "Log In"',
+                    '6. Find the Star Math or Reading test (depending on teacher '
+                    'directions).',
+                    '7. Begin your test. If you need to enter a monitor password, '
+                    'type <b>Admin</b>',
+                    '',
+                    '<span style="color: red;">STAY ON THE ZOOM UNTIL WE CONFIRM '
+                    'YOU SUBMITTED YOUR TEST</span>',
+                    '',
+                    'Good luck!',
+                ]
+                eml.send(
+                    to=recipient,
+                    subject='Star360 Username and Password',
+                    message=message
+                )
+                self.sent_to.append(st.name)
+                logger.info(f'Email sent to {st.name} including username: {st.username}')
+                logger.debug(f'{i}/{len(self.students)} emails sent')
+                sent_to.append(st)
