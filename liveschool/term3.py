b4fe30a664004bd56396793f8fbb6026c4a9c754
Jack DeVries <jdevries3133@gmail.com>
2022-04-25 13:27:30 -0400

    create liveschool module

---
+++ liveschool/term3.py
@@ -0,0 +1,8 @@
+"""Module for parsing LiveSchool data according to term 3 rules."""
+
+from teacherhelper import Sis
+
+
+class Term3Parser
+    def __init__(self) -> None:
+        self.sis = Sis.read_cache()
