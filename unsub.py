c8c7c6b0de643c163e935e1468312b8a92119d55
Jack DeVries <jdevries3133@gmail.com>
2024-11-22 21:58:26 -0500

    feat: send GET to retry unsub

--- unsub.py
+++ unsub.py
@@ -5,22 +5,29 @@
 import email
 import sys
 import urllib.request
+from urllib.error import HTTPError
 
 msg = email.message_from_string(sys.stdin.read())
 for header in msg.items():
-    if header[0] == 'List-Unsubscribe':
-        links = header[1].split(',')
+    if header[0] == "List-Unsubscribe":
+        links = header[1].split(",")
         for link in links:
-            link = link.rstrip('>')
-            link = link.lstrip('<')
-            if not link.startswith('https'):
+            link = link.rstrip(">")
+            link = link.lstrip("<")
+            if not link.startswith("https"):
                 continue
 
-            req = urllib.request.Request(link, method="POST")
-            with urllib.request.urlopen(req) as response:
-                print(response.read().decode('utf-8'))
-                exit(0)
+            try:
+                req = urllib.request.Request(link, method="POST")
+                with urllib.request.urlopen(req) as response:
+                    print(response.read().decode("utf-8"))
+                    exit(0)
+            except HTTPError as e:
+                print("Retrying with GET")
+                req = urllib.request.Request(link, method="GET")
+                with urllib.request.urlopen(req) as response:
+                    print(response.read().decode("utf-8"))
+                    exit(0)
 
-print('Could not unsubscribe.')
+print("Could not unsubscribe.")
 exit(1)
-
